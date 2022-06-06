6bf5fc69ce90bb1fb2ffb0ab640416aa4d4b4af3
yesoer <luisenglaender@web.de>
2022-06-06 21:59:59 +0200

    implementation of login route

--- router.go
+++ router.go
@@ -3,11 +3,13 @@ package main
 import (
 	"os"
 
+	"github.com/PPSKSY-Cluster/backend/auth"
 	_ "github.com/PPSKSY-Cluster/backend/docs"
 	"github.com/PPSKSY-Cluster/backend/handlers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/swagger"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func InitRouter() (*fiber.App, error) {
@@ -24,6 +26,7 @@ func InitRouter() (*fiber.App, error) {
 	api := router.Group("/api")
 	api.Get("/ping", pingHandler())
 	api.Get("/docs/*", docsHandler())
+	api.Post("/login", loginHandler())
 
 	userRoutes := api.Group("/users")
 	handlers.InitUserHandlers(userRoutes)
@@ -32,6 +35,38 @@ func InitRouter() (*fiber.App, error) {
 	handlers.InitCResourceHandlers(cResourceRoutes)
 
 	return router, nil
+}
+
+// @Description  Route for login
+// @Tags         general
+// @Accept       json
+// @Produce      json
+// @Param        username   body   string  true  "Username"
+// @Param        password   body   string  true  "Password"
+// @Success      200  {string} string
+// @Success      401  {}
+// @Router       /api/login [post]
+func loginHandler() func(c *fiber.Ctx) error {
+	type LoginPair struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	return func(c *fiber.Ctx) error {
+		var login LoginPair
+
+		if err := c.BodyParser(&login); err != nil {
+			return c.SendStatus(500)
+		}
+
+		token, err := auth.CheckCredentials(login.Username, login.Password)
+		if err != nil {
+			return c.SendStatus(401)
+		}
+
+		c.JSON(bson.M{"token": token})
+		return c.SendStatus(200)
+	}
 }
 
 // @Description  The route that serves the swagger documentation
