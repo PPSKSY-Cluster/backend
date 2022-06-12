c0a96989c37deb4637c19d00bcfe24bc11856516
vDawgg <veit.laule@gmail.com>
2022-06-12 17:09:18 +0200

    Completed resource handlers

--- handlers/c_resources.go
+++ handlers/c_resources.go
@@ -1,10 +1,11 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
-
-type CResource struct {
-	ID string `json:"_id"`
-}
+import (
+	"github.com/PPSKSY-Cluster/backend/db"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func InitCResourceHandlers(cresourceRouter fiber.Router) {
 	cresourceRouter.Get("/", cresourceListHandler())
@@ -20,10 +21,16 @@ func InitCResourceHandlers(cresourceRouter fiber.Router) {
 // @Tags         cluster-resources
 // @Produce      json
 // @Success      200  {array}  CResource
+// @Failure		 500
 // @Router       /api/cresources/ [get]
 func cresourceListHandler() func(c *fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
-		c.JSON([]CResource{})
+		cResources, err := db.GetAllResources()
+		if err != nil {
+			return c.SendStatus(500)
+		}
+
+		c.JSON(cResources)
 		return c.SendStatus(200)
 	}
 }
@@ -33,10 +40,22 @@ func cresourceListHandler() func(c *fiber.Ctx) error {
 // @Produce      json
 // @Param        id   path      string  true  "CResource ID"
 // @Success      200  {object}  CResource
+// @Failure 	 404
 // @Router       /api/cresources/{id} [get]
 func cresourceDetailHandler() func(c *fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
-		c.JSON(CResource{})
+		idStr := c.Params("id")
+		id, err := primitive.ObjectIDFromHex(idStr)
+		if err != nil {
+			return c.SendStatus(500)
+		}
+
+		cResource, err := db.GetResourceById(id)
+		if err != nil {
+			return c.SendStatus(404)
+		}
+
+		c.JSON(cResource)
 		return c.SendStatus(200)
 	}
 }
@@ -46,10 +65,23 @@ func cresourceDetailHandler() func(c *fiber.Ctx) error {
 // @Accept       json
 // @Produce      json
 // @Success      201  {object}  CResource
+// @Failure      500  {object}  string
 // @Router       /api/cresources/ [post]
 func cresourceCreateHandler() func(c *fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
-		c.JSON(CResource{})
+		cr := new(db.CResource)
+		if err := c.BodyParser(cr); err != nil {
+			c.JSON(bson.M{"error": err.Error()})
+			return c.SendStatus(500)
+		}
+
+		cResource, err := db.AddResource(*cr)
+		if err != nil {
+			c.JSON(bson.M{"error": err.Error()})
+			return c.SendStatus(500)
+		}
+
+		c.JSON(cResource)
 		return c.SendStatus(201)
 	}
 }
@@ -60,10 +92,27 @@ func cresourceCreateHandler() func(c *fiber.Ctx) error {
 // @Produce      json
 // @Param        id   path      string  true  "CResource ID"
 // @Success      200  {object}  CResource
+// @Failure      500
 // @Router       /api/cresources/{id} [put]
 func cresourceUpdateHandler() func(c *fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
-		c.JSON(CResource{})
+		cr := new(db.CResource)
+		if err := c.BodyParser(cr); err != nil {
+			return c.SendStatus(500)
+		}
+
+		idStr := c.Params("id")
+		id, err := primitive.ObjectIDFromHex(idStr)
+		if err != nil {
+			return c.SendStatus(500)
+		}
+
+		user, err := db.EditResource(id, *cr)
+		if err != nil {
+			return c.SendStatus(500)
+		}
+
+		c.JSON(user)
 		return c.SendStatus(200)
 	}
 }
@@ -72,9 +121,21 @@ func cresourceUpdateHandler() func(c *fiber.Ctx) error {
 // @Tags         cluster-resources
 // @Param        id   path      string  true  "CResource ID"
 // @Success      204
+// @Failure      500
 // @Router       /api/cresources/{id} [delete]
 func cresourceDeleteHandler() func(c *fiber.Ctx) error {
 	return func(c *fiber.Ctx) error {
+		idStr := c.Params("id")
+		id, err := primitive.ObjectIDFromHex(idStr)
+		if err != nil {
+			return c.SendStatus(500)
+		}
+
+		err = db.DeleteUser(id)
+		if err != nil {
+			return c.SendStatus(500)
+		}
+
 		return c.SendStatus(204)
 	}
 }
